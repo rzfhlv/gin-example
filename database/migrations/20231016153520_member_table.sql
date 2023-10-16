a28170aeac7a19fe5a20a440ee4c1d1466cb26fb
rzfhlv <rzfhlv.me@gmail.com>
2023-10-17 00:06:03 +0700

    add rest of migrations

--- database/migrations/20231016153520_member_table.sql
+++ database/migrations/20231016153520_member_table.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS members (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT UNSIGNED AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     UNIQUE(email),
     PRIMARY KEY(id)
 );
---
+++ database/migrations/20231017000346_attendee_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS attendee (
+    member_id BIGINT UNSIGNED,
+    gathering_id BIGINT UNSIGNED,
+
+    FOREIGN KEY (member_id) REFERENCES members(id)
+    FOREIGN KEY (gathering_id) REFERENCES gatherings(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS attendee;
+-- +goose StatementEnd
