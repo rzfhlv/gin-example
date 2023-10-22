5cae37e818b94aa8c36ae02bb16f1de686db1050
rzfhlv <rzfhlv.me@gmail.com>
2023-10-22 23:54:04 +0700

    add user unit test

--- internal/modules/user/handler/handler_test.go
+++ internal/modules/user/handler/handler_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"database/sql"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -15,9 +16,9 @@ import (
 )
 
 type testCase struct {
-	name, body, param, queryParam string
-	wantError                     error
-	code                          int
+	name, body, param, queryParam, setContext string
+	wantError                                 error
+	code                                      int
 }
 
 var (
@@ -99,6 +100,118 @@ func TestLogin(t *testing.T) {
 
 			h.Login(ctx)
 			assert.EqualValues(t, tt.code, w.Code)
+		})
+	}
+}
+
+func TestLogout(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	testCase := []testCase{
+		{
+			name: "Testcase #1: Positive", body: "Bearer thisistoken", wantError: nil, setContext: "username", code: http.StatusOK,
+		},
+		{
+			name: "Testcase #2: Negative", body: "Bearer", wantError: nil, setContext: "username", code: http.StatusUnauthorized,
+		},
+		{
+			name: "Testcase #3: Negative", body: "Bearer thisistoken", wantError: nil, setContext: "any", code: http.StatusUnauthorized,
+		},
+		{
+			name: "Testcase #4: Negative", body: "Bearer thisistoken", wantError: errFoo, setContext: "username", code: http.StatusInternalServerError,
+		},
+	}
+	for _, tt := range testCase {
+		t.Run(tt.name, func(t *testing.T) {
+			mockUsecase := mockUsecase.IUsecase{}
+			mockUsecase.On("Logout", mock.Anything, mock.Anything).Return(tt.wantError)
+
+			h := &Handler{
+				usecase: &mockUsecase,
+			}
+
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+			ctx.Request = httptest.NewRequest(http.MethodPost, "/v1/users/logout", nil)
+			ctx.Request.Header.Set("Content-Type", "application/json")
+			ctx.Request.Header.Set("Authorization", tt.body)
+			ctx.Set(tt.setContext, "johndoe")
+
+			h.Logout(ctx)
+			assert.EqualValues(t, tt.code, w.Code)
+		})
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	expectedCount := int64(10)
+	testCase := []testCase{
+		{
+			name: "Testcase #1: Positive", queryParam: "?page=1", wantError: nil, code: http.StatusOK,
+		},
+		{
+			name: "Testcase #2: Negative", queryParam: "?page=1", wantError: errFoo, code: http.StatusInternalServerError,
+		},
+		{
+			name: "Testcase #3: Negative", queryParam: "?page=one", wantError: errFoo, code: http.StatusUnprocessableEntity,
+		},
+	}
+	for _, tt := range testCase {
+		t.Run(tt.name, func(t *testing.T) {
+			mockUsecase := mockUsecase.IUsecase{}
+			mockUsecase.On("GetAll", mock.Anything, mock.Anything).Return([]model.User{}, expectedCount, tt.wantError)
+
+			h := &Handler{
+				usecase: &mockUsecase,
+			}
+
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+			ctx.Request = httptest.NewRequest(http.MethodGet, "/v1/users"+tt.queryParam, nil)
+			ctx.Request.Header.Set("Content-Type", "application/json")
+
+			h.GetAll(ctx)
+			assert.Equal(t, tt.code, w.Code)
+		})
+	}
+}
+
+func TestGetByID(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	testCase := []testCase{
+		{
+			name: "Testcase #1: Positive", param: "1", wantError: nil, code: http.StatusOK,
+		},
+		{
+			name: "Testcase #2: Negative", param: "1", wantError: errFoo, code: http.StatusInternalServerError,
+		},
+		{
+			name: "Testcase #3: Negative", param: "one", wantError: errFoo, code: http.StatusUnprocessableEntity,
+		},
+		{
+			name: "Testcase #3: Negative", param: "0", wantError: sql.ErrNoRows, code: http.StatusNotFound,
+		},
+	}
+	for _, tt := range testCase {
+		t.Run(tt.name, func(t *testing.T) {
+			mockUsecase := mockUsecase.IUsecase{}
+			mockUsecase.On("GetByID", mock.Anything, mock.Anything).Return(model.User{}, tt.wantError)
+
+			h := &Handler{
+				usecase: &mockUsecase,
+			}
+
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+			ctx.Request = httptest.NewRequest(http.MethodGet, "/v1/users/"+tt.param, nil)
+			ctx.Request.Header.Set("Content-Type", "application/json")
+			ctx.Params = gin.Params{{Key: "id", Value: tt.param}}
+
+			h.GetByID(ctx)
+			assert.Equal(t, tt.code, w.Code)
 		})
 	}
 }
---
+++ internal/modules/user/repository/repository_test.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-redis/redismock/v9"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	// now   = time.Date(2023, time.August, 15, 12, 0, 0, 0, time.UTC)
+	ctx   = context.Background()
+	ttl   = time.Duration(1 * time.Hour)
+	key   = "testKey"
+	value = "testValue"
+)
+
+func TestNew(t *testing.T) {
+	mockDB, _, _ := sqlmock.New()
+	defer mockDB.Close()
+
+	db := sqlx.NewDb(mockDB, "sqlmock")
+
+	client, _ := redismock.NewClientMock()
+
+	r := New(db, client)
+	assert.NotNil(t, r)
+}
